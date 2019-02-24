6ccb0331975964d4ffa98d8744801965df4ec36c
Felix Breuer <webmaster@scriptworld.net>
2019-02-24 16:20:06 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-npm install
-pip3 install -r requirements.txt
-pip3 install -I https://github.com/timgrossmann/InstaPy/zipball/signature-types
-cd services && python3 get_actions.py && python3 config_service.py
