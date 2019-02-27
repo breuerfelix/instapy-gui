cc95434c6123f14496e1729240a0adfb4a4fd837
felixbreuer <i@felixbreuer.me>
2019-02-27 18:19:12 +0100

    added dashboard cards

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import SideBar from './sidebar';
 import Footer from './footer';
 import MenuItem from './menu_item';
 import Location from './location';
+import IframeCard from './iframe_card';
 
 export {
 	NavBar,
 	SideBar,
 	Footer,
 	MenuItem,
-	Location
+	Location,
+	IframeCard
 };
