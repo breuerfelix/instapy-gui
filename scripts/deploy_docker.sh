2f4a93269da8a2510a638a7d08f130f366131936
Felix Breuer <f.breuer94@gmail.com>
2019-06-18 17:42:04 +0200

    clean travis build docker

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,7 +1,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push felixbreuer/instapy-webserver:latest
 docker push felixbreuer/instapy-config:latest
 docker push felixbreuer/instapy-auth:latest
 docker push felixbreuer/instapy-socket:latest
-docker push felixbreuer/instapy:latest
-docker push felixbreuer/instapy-grafana:latest
