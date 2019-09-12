64863ed0b563f71b1314b6e22384deff07719ca1
felixbreuer <hi@felixbreuer.me>
2019-09-12 11:09:15 +0200

    fix security issue with target=_blank

--- src/sites/home.jsx
+++ src/sites/home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
 		<p style={{ padding: '10px 3%' }}>
 			<h2 align='center'>InstaPy GUI</h2>
 			<p align='center'>
-				<a href='https://github.com/breuerfelix/instapy-gui' target='__blank'>Open Source</a> Graphical User Interface for <a href='https://github.com/timgrossmann/InstaPy'>InstaPy</a> automation.
+				<a rel='noopener noreferrer' href='https://github.com/breuerfelix/instapy-gui' target='_blank'>Open Source</a> Graphical User Interface for <a href='https://github.com/timgrossmann/InstaPy'>InstaPy</a> automation.
 				<br/>
 				<b>
 					Get real followers, likes and comments using free Open Source tools!
@@ -18,9 +18,9 @@ const Home = () => (
 				<h6>How to get started within 10 minutes:</h6>
 				<br/>
 				<ol>
-					<li>Install <a target='_blank' href='https://www.mozilla.org'>Firefox</a></li>
+					<li>Install <a rel='noopener noreferrer' target='_blank' href='https://www.mozilla.org'>Firefox</a></li>
 					<li>Create an <Link to='/login'>Account</Link></li>
-					<li><a target='_blank' href='https://github.com/breuerfelix/instapy-gui'>Register</a> a Bot</li>
+					<li><a rel='noopener noreferrer' target='_blank' href='https://github.com/breuerfelix/instapy-gui'>Register</a> a Bot</li>
 					<li>Create a <Link to='/configuration/namespaces'>Template</Link></li>
 					<li>Modify your <Link to='/configuration/settings'>Settings</Link></li>
 					<li><Link to='/start'>Start</Link> your automation tool!</li>
